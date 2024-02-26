8c3067f4fefb8ddb42b1fcbd1fffaed7e40099d0
JerrySabino <jer.lsabino@gmail.com>
2024-02-26 17:12:26 -0400

    Add controller functionality for Users and Notes

---
+++ controllers/notesController.js
@@ -0,0 +1,115 @@
+const User = require('../models/User')
+const Note = require('../models/Note')
+
+const asyncHandler = require('express-async-handler') // Middleware to keep us from using too many try-catch blocks
+const bcrypt = require('bcrypt') // Password encrypting
+
+// @desc    Get all notes
+// @route   GET /notes
+// @access  Private
+const getAllNotes = asyncHandler(async (req, res) => {
+  //Get all 
+  const notes = await Note.find().lean()
+
+  //If none
+  if (!notes?.length) {
+    return res.status(400).json({ message: 'No notes found'})
+  }
+
+  res.json(notes)
+})
+
+// @desc    Create a new note
+// @route   POST /notes
+// @access  Private
+const createNewNote = asyncHandler(async (req, res) => {
+  const { user, title, text } = req.body
+
+  //Confirm data
+  if(!user || !title || !text ) {
+    return res.status(400).json({ message: 'Please fill in all fields' })
+  }
+
+  //Check duplicates
+  const duplicate = await Note.findOne({ title }).lean().exec()
+
+  if (duplicate) {
+    return res.status(400).json({ message: 'Note with the same title already exists' })
+  }
+
+  //Create and store new note
+  const noteObject = { user, title, text }
+
+  const note = await Note.create(noteObject)
+
+  if (note) { //created
+    res.status(201).json({ message: `New note '${note.title}' created`})
+  } else {
+    res.status(400).json({ message: 'Invalid note data received' })
+  }
+
+})
+
+// @desc    Update a note
+// @route   PATCH /notes
+// @access  Private
+const updateNote = asyncHandler(async (req, res) => {
+  const { id, user, title, text, completed } = req.body
+
+  //Confirm data
+  if(!id || !user || !title || !text ) {
+    return res.status(400).json({ message: 'Please fill in all fields' })
+  }
+
+  const note = await Note.findById(id).exec()
+
+  //Check duplicate
+  const duplicate = await User.findOne({ title }).lean().exec()
+
+  //Allow updates to original note
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: 'Duplicate note' })
+  }
+
+  //Update note with new data
+  note.user = user
+  note.title = title
+  note.text = text
+
+  const updatedNote = await note.save()
+
+  res.json({ message: `${updatedNote.title} updated`})
+})
+
+
+// @desc    Delete a note
+// @route   DELETE /notes
+// @access  Private
+const deleteNote= asyncHandler(async (req, res) => {
+  const { id } = req.body
+
+  //Confirm data
+  if (!id) {
+    return res.status(400).json({ message: 'Note id required' })
+  }
+
+  const note = await Note.findById(id).exec()
+
+  if (!note) {
+    return res.status(400).json({ message: 'Note not found' })
+  }
+
+  //Delete note
+  const result = await note.deleteOne()
+
+  const reply = `Note '${note.title}' with ID ${note._id} deleted`
+
+  res.json(reply)
+})
+
+module.exports = {
+  getAllNotes,
+  createNewNote,
+  updateNote,
+  deleteNote
+}
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
-const User = require('./models/User')
+const User = require('../models/User')
 const Note = require('../models/Note')
+
 const asyncHandler = require('express-async-handler') // Middleware to keep us from using too many try-catch blocks
 const bcrypt = require('bcrypt') // Password encrypting
 
@@ -7,17 +8,21 @@ const bcrypt = require('bcrypt') // Password encrypting
 // @route   GET /users
 // @access  Private
 const getAllUsers = asyncHandler(async (req, res) => {
+  //Get all 
   const users = await User.find().select('-password').lean()
-  if (!users) {
+
+  //If none
+  if (!users?.length) {
     return res.status(400).json({ message: 'No users found'})
   }
+
   res.json(users)
 })
 
 // @desc    Create new user
 // @route   POST /users
 // @access  Private
-const createNewUser= asyncHandler(async (req, res) => {
+const createNewUser = asyncHandler(async (req, res) => {
   const { username, password, roles } = req.body
 
   //Confirm data
@@ -30,27 +35,29 @@ const createNewUser= asyncHandler(async (req, res) => {
 
   if (duplicate) {
     return res.status(409).json({ message: 'Duplicate username' })
-
-    //Hash password
-    const hashedPwd = await bcrypt.hash(password, 10) // Add 10 salt rounds (hashes the password)
-
-    const userObject = { username, "password": hashedPwd, roles }
-    
-    //Create and store new user
-    const user = await User.create(useObject)
-
-    if (user) { //created
-      res.status(201).json({ message: `New user ${username} created`})
-    } else {
-      res.status(400).json({ message: 'Invalid user data received'})
-    }
   }
+
+  //Hash password
+  const hashedPwd = await bcrypt.hash(password, 10) // Add 10 salt rounds (hashes the password)
+
+  
+  //Create and store new user
+  const userObject = { username, "password": hashedPwd, roles }
+  
+  const user = await User.create(userObject)
+
+  if (user) { //created
+    res.status(201).json({ message: `New user ${username} created`})
+  } else {
+    res.status(400).json({ message: 'Invalid user data received'})
+  }
+  
 })
 
 // @desc    Update a user
-// @route   POST /users
+// @route   PATCH /users
 // @access  Private
-const updateUser= asyncHandler(async (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   const { id, username, roles, active, password } = req.body
   
   //Confirm data
@@ -88,17 +95,19 @@ const updateUser= asyncHandler(async (req, res) => {
 })
 
 // @desc    Delete a user
-// @route   POST /users
+// @route   DELETE /users
 // @access  Private
-const deleteUser= asyncHandler(async (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   const { id } = req.body
 
+  //Confirm data
   if (!id) {
     return res.status(400).json({ message: 'User id required' })
   }
 
-  const notes = await Note.findOne({ user: id }).lean().exec()
-  if (notes?.length) {
+  // Check if user has any assigned notes
+  const note = await Note.findOne({ user: id }).lean().exec()
+  if (note) {
     return res.status(400).json({ messages: 'User has assigned notes'})
   }
 
@@ -108,9 +117,17 @@ const deleteUser= asyncHandler(async (req, res) => {
     return res.status(400).json({ message: 'User not found' })
   }
 
+  //Delete user
   const result = await user.deleteOne()
 
-  const reply = `Username ${result.username} with ID ${result._id} deleted`
+  const reply = `Username ${user.username} with ID ${user._id} deleted`
 
   res.json(reply)
 })
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser
+}
